a11f644e5cb8c2f2ba5f33189c54d122f4821b71
cgi-bin <s@ono.re>
2023-12-23 18:28:35 +0100

    add: env

--- dist/scripts/01_deploy_dynamicArray.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ts_1 = require("../artifacts/ts");
-const deployDynamicArray = async (deployer) => {
-    await deployer.deployContract(ts_1.DynamicArrayForInt, {
-        initialFields: {},
-    });
-};
-exports.default = deployDynamicArray;
