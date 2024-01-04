400d537460739acdb5f0f9b597b838afc990d69a
cgi-bin/ <6095048+sven-hash@users.noreply.github.com>
2024-01-04 09:43:41 +0100

    fix: missing alef

--- src/bot.ts
+++ src/bot.ts
@@ -77,7 +77,7 @@ const optionsBear: EventSubscribeOptions<PredictalphTypes.BetBearEvent> = {
      sendMessage(
         bot,
         chatId,
-        `\n\n🐻 Round ${event.fields.epoch} - <b>New Bear</b> in the room.\n🎫 Bet ${(Number(event.fields.amount-ONE_ALPH) / Number(ONE_ALPH)).toFixed(3)}\n\nWant to bet against ? <a href="https://alph.bet">Play here</a>`
+        `\n\n🐻 Round ${event.fields.epoch} - <b>New Bear</b> in the room.\n🎫 Bet ${(Number(event.fields.amount-ONE_ALPH) / Number(ONE_ALPH)).toFixed(3)}${ALPH}\n\nWant to bet against ? <a href="https://alph.bet">Play here</a>`
       );
       sendTweet(twitterClient, `🐻 Round ${event.fields.epoch} - New Bear in the room.\n🎫 Bet ${(Number(event.fields.amount-ONE_ALPH) / Number(ONE_ALPH)).toFixed(3)}${ALPH}\n\nWant to bet against ? Play here 👇 https://alph.bet\n#alephium #dapp ${randomChar()}` )
       bearCounter++;
